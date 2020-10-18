e109305f8c0381451d9af40adaafe1b7e51aea65
f10lla <badjokeskiller@gmail.com>
2020-10-18 13:18:09 +0300

    v2

--- coffee/coffee/coffee.py
+++ coffee/coffee/coffee.py
@@ -7,7 +7,7 @@ while 0 == 0:
         print('Выберите кофе:\n1.Американо - 60р\n2.Латте - 80р\n3.Каппучино - 40р\n4.Экспрессо - 50р\n5.Двойной экспрессо - 70р\n6.Горячий шоколад - 60р')
         try:
             t = int(input())
-        except ValueError:
+        except ValueError: 
             print("\n")
             continue
         break
@@ -15,8 +15,14 @@ while 0 == 0:
     p = [60, 80, 40, 50, 70, 60]
     
     while t < 1 or t > 6:
-        print("\nВведите число от одного до шести")
-        t = int(input())
+        while 0 == 0:
+            print('Введите число от одного до шести...')
+            try:
+                t = int(input())
+            except ValueError: 
+                print("\n")
+                continue
+            break
     while 0 == 0:
         print("\nВнесите " + str(p[t-1]) + " рублей")
         try:
@@ -33,11 +39,10 @@ while 0 == 0:
             while 0 == 0:
                 print("\nНедостаточно средств на балансе... Добавьте еще " + str(p[t-1] - m) + "р")
                 try:
-                    a = int(input())
+                    m += int(input())
                 except ValueError:
                     continue
                 break
-            m += a
 
     print("\nТвой кофе уже готовится...\n")
 
